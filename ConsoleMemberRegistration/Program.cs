67bf8bd2a82946b69d963f4ac31bc5cd3030d8c5
Mariana <mariana_taru@yahoo.com.br>
2022-02-17 22:04:01 +1100

    .

--- ConsoleMemberRegistration/Program.cs
+++ ConsoleMemberRegistration/Program.cs
@@ -18,159 +18,169 @@ namespace ConsoleMemberRegistration
             Members members = new Members();
             string result = "";
 
-            Console.WriteLine("Type 1 to register a new member, 2 to search for existing member, 3 to delete a member or 4 to update:");
-            option = Convert.ToInt16(Console.ReadLine());
-
-            //Register a new member
-            if (option == 1)
+            while (option != 5)
             {
-                string isbaptized = "";
-                Console.WriteLine("Member Registration");
-                try
+                Console.WriteLine(
+                    "Type 1 to register a new member, 2 to search, 3 to delete, 4 to update or 5 to exit:");
+                option = Convert.ToInt16(Console.ReadLine());
+
+                //Register a new member
+                if (option == 1)
                 {
-                    Console.WriteLine("Type member name:");
-                    members.Name = Convert.ToString(Console.ReadLine());
-                    Console.WriteLine("Email:");
-                    members.Email = Convert.ToString(Console.ReadLine());
-                    Console.WriteLine("Phone:");
-                    members.Phone = Convert.ToString(Console.ReadLine());
-                    Console.WriteLine("Birth of Date: (2000/01/24)");
-                    members.BirthDate = Convert.ToDateTime(Console.ReadLine());
-                    Console.WriteLine("IsBaptized?: (yes/no)");
-                    isbaptized = Convert.ToString(Console.ReadLine());
-                    if (isbaptized.ToLower() == "yes")
+                    string isbaptized = "";
+                    Console.WriteLine("Member Registration");
+                    try
                     {
-                        members.IsBaptized = true;
-                    }
-                    else
-                    {
-                        members.IsBaptized = false;
-                    }
-                    Console.WriteLine("Nationality:");
-                    members.Nationality = Convert.ToString(Console.ReadLine());
-                    members.DateOfRegister = DateTime.Now;
-
-                    result = members.InsertMember(members, connectionString);
-                    
-                    Console.WriteLine(result);
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                    throw;
-                }
-            }
-
-            //search for existing member
-            else if (option == 2)
-            {
-                try
-                {
-                    string nMember = "";
-                    Console.WriteLine("Type member name:");
-                    nMember = Console.ReadLine();
-                    List<Members> memberList = new List<Members>();
-
-                    memberList = members.ViewMember(nMember, connectionString);
-
-                    if (memberList != null &&  memberList.Count != 0  )
-                    {
-                        foreach (var mbr in memberList)
+                        Console.WriteLine("Type member name:");
+                        members.Name = Convert.ToString(Console.ReadLine());
+                        Console.WriteLine("Email:");
+                        members.Email = Convert.ToString(Console.ReadLine());
+                        Console.WriteLine("Phone:");
+                        members.Phone = Convert.ToString(Console.ReadLine());
+                        Console.WriteLine("Birth of Date: (2000/01/24)");
+                        members.BirthDate = Convert.ToDateTime(Console.ReadLine());
+                        Console.WriteLine("IsBaptized?: (yes/no)");
+                        isbaptized = Convert.ToString(Console.ReadLine());
+                        if (isbaptized.ToLower() == "yes")
                         {
-                            Console.Write("Member code: ");
-                            Console.WriteLine(mbr.CodMember);
-                            Console.Write("Name: ");
-                            Console.WriteLine(mbr.Name);
-                            Console.Write("Email: ");
-                            Console.WriteLine(mbr.Email);
-                            Console.Write("Phone number: ");
-                            Console.WriteLine(mbr.Phone);
-                            Console.Write("Date of Birth: ");
-                            Console.WriteLine(mbr.BirthDate);
-                            Console.Write("Nationality: ");
-                            Console.WriteLine(mbr.Nationality);
-                            Console.Write("Member is baptized: ");
-                            Console.WriteLine(mbr.IsBaptized);
-                            Console.Write("Date of Register: ");
-                            Console.WriteLine(mbr.DateOfRegister);
-                            Console.WriteLine("----------------------------------- ");
+                            members.IsBaptized = true;
+                        }
+                        else
+                        {
+                            members.IsBaptized = false;
+                        }
+
+                        Console.WriteLine("Nationality:");
+                        members.Nationality = Convert.ToString(Console.ReadLine());
+                        members.DateOfRegister = DateTime.Now;
+
+                        result = members.InsertMember(members, connectionString);
+
+                        Console.WriteLine(result);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                        throw;
+                    }
+                }
+
+                //search for existing member
+                else if (option == 2)
+                {
+                    try
+                    {
+                        string nMember = "";
+                        Console.WriteLine("Type member name:");
+                        nMember = Console.ReadLine();
+                        List<Members> memberList = new List<Members>();
+
+                        memberList = members.ViewMember(nMember, connectionString);
+
+                        if (memberList != null && memberList.Count != 0)
+                        {
+                            foreach (var mbr in memberList)
+                            {
+                                Console.Write("Member code: ");
+                                Console.WriteLine(mbr.CodMember);
+                                Console.Write("Name: ");
+                                Console.WriteLine(mbr.Name);
+                                Console.Write("Email: ");
+                                Console.WriteLine(mbr.Email);
+                                Console.Write("Phone number: ");
+                                Console.WriteLine(mbr.Phone);
+                                Console.Write("Date of Birth: ");
+                                Console.WriteLine(mbr.BirthDate);
+                                Console.Write("Nationality: ");
+                                Console.WriteLine(mbr.Nationality);
+                                Console.Write("Member is baptized: ");
+                                Console.WriteLine(mbr.IsBaptized);
+                                Console.Write("Date of Register: ");
+                                Console.WriteLine(mbr.DateOfRegister);
+                                Console.WriteLine("----------------------------------- ");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Member not found.");
                         }
                     }
-                    else
+                    catch (Exception e)
                     {
-                        Console.WriteLine("Member not found.");
+                        Console.WriteLine(e.Message);
+                        throw;
                     }
                 }
-                catch (Exception e)
+
+                //delete a existing member
+                else if (option == 3)
                 {
-                    Console.WriteLine(e.Message);
-                    throw;
+                    try
+                    {
+                        int code = 0;
+                        Console.WriteLine("Enter member's code:");
+                        code = Convert.ToInt32(Console.ReadLine());
+
+                        result = members.DeleteMembers(code, connectionString);
+
+                        Console.WriteLine(result);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                        throw;
+                    }
                 }
-            }
-    
-            //delete a existing member
-            else if (option == 3)
-            {
-                try
+
+                //update a existing member
+                else if (option == 4)
                 {
                     int code = 0;
-                    Console.WriteLine("Enter member's code:");
-                    code = Convert.ToInt32(Console.ReadLine());
-
-                    result = members.DeleteMembers(code, connectionString);
-                    
-                    Console.WriteLine(result);
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                    throw;
-                }
-            }
-
-            //update a existing member
-            else if (option == 4)
-            {
-                int code = 0;
-                string isbaptized = "";
-                try
-                {
-                    Console.WriteLine("Type the code of the member you want to update:");
-                    code = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("Name");
-                    members.Name = Convert.ToString(Console.ReadLine());
-                    Console.WriteLine("Email:");
-                    members.Email = Convert.ToString(Console.ReadLine());
-                    Console.WriteLine("Phone:");
-                    members.Phone = Convert.ToString(Console.ReadLine());
-                    Console.WriteLine("Birth of Date: (2000/01/24)");
-                    members.BirthDate = Convert.ToDateTime(Console.ReadLine());
-                    Console.WriteLine("IsBaptized?: (yes/no)");
-                    isbaptized = Convert.ToString(Console.ReadLine());
-                    if (isbaptized.ToLower() == "yes")
+                    string isbaptized = "";
+                    try
                     {
-                        members.IsBaptized = true;
-                    }
-                    else
-                    {
-                        members.IsBaptized = false;
-                    }
-                    Console.WriteLine("Nationality:");
-                    members.Nationality = Convert.ToString(Console.ReadLine());
-                    members.DateOfRegister = DateTime.Now;
+                        Console.WriteLine("Type the code of the member you want to update:");
+                        code = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Name");
+                        members.Name = Convert.ToString(Console.ReadLine());
+                        Console.WriteLine("Email:");
+                        members.Email = Convert.ToString(Console.ReadLine());
+                        Console.WriteLine("Phone:");
+                        members.Phone = Convert.ToString(Console.ReadLine());
+                        Console.WriteLine("Birth of Date: (2000/01/24)");
+                        members.BirthDate = Convert.ToDateTime(Console.ReadLine());
+                        Console.WriteLine("IsBaptized?: (yes/no)");
+                        isbaptized = Convert.ToString(Console.ReadLine());
+                        if (isbaptized.ToLower() == "yes")
+                        {
+                            members.IsBaptized = true;
+                        }
+                        else
+                        {
+                            members.IsBaptized = false;
+                        }
 
-                    result = members.EditMember(code, members, connectionString);
-                    Console.WriteLine(result);
+                        Console.WriteLine("Nationality:");
+                        members.Nationality = Convert.ToString(Console.ReadLine());
+                        members.DateOfRegister = DateTime.Now;
+
+                        result = members.EditMember(code, members, connectionString);
+                        Console.WriteLine(result);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                        throw;
+                    }
                 }
-                catch (Exception e)
+                else if (option == 5)
                 {
-                    Console.WriteLine(e.Message);
-                    throw;
+                    break;
                 }
-            }
-            else 
-            {
-                Console.WriteLine("Invalid option!");
+                else
+                {
+                    Console.WriteLine("Invalid option!");
+                }
             }
         }
     }
